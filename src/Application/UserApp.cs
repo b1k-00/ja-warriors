247708a7a30691782494e8514376b05eca2e5ac5
Brennen Collins <brennen.collins@ruralsourcing.com>
2023-02-09 11:51:06 -0600

    added controller that will hande CRUD operations for users

---
+++ src/Application/UserApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Application;
+public class UserApp
+//a.m    type    name
+{
+    static List<User> users = new List<User>();
+    //a.m   type        name      
+    
+
+    //understand why we used static instead
+
+    public static List<User> GetUsers() { return users; }
+    //access mod    type        method       parameters
+}
