97dff06dbb4833872b6e89a79c88e40f06f3bed3
Libby Blair <libby.blair@ruralsourcing.com>
2023-02-14 12:53:42 -0600

    Created Role class; completed User class; changed Create, Drop, and Seed SQL scripts to insert a User and Role table and values.

--- src/Persistence/Scripts/Create.sql
+++ src/Persistence/Scripts/Create.sql
@@ -1,24 +1,56 @@
-﻿/****** this query will create a table called 'Users' on the 'JAScheduler' database *****/
+﻿/****** this query will create a table called 'User' on the 'JAScheduler' database *****/
 
 USE [JAScheduler]
 GO
 
-/****** Object:  Table [dbo].[Users]    Script Date: 2/7/2023 9:36:15 AM ******/
+/****** Object:  Table [dbo].[User]    Script Date: 2/14/2023 11:22:13 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Users](
+CREATE TABLE [dbo].[User](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[FirstName] [varchar](50) NULL,
-	[LastName] [varchar](50) NULL,
 	[MiddleName] [varchar](50) NULL,
+	[LastName] [varchar](50) NULL,
 	[Email] [varchar](100) NULL,
 	[Password] [varchar](50) NULL,
 	[PhoneNumber] [varchar](22) NULL,
 	[Graduated] [bit] NULL,
-	[GraduatedDate] [date] NULL
+	[GraduatedDate] [date] NULL,
+	[RoleId] [int] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([Id])
+GO
+
+
+-- This query will create a table "Role" in the "JAScheduler" database
+
+USE [JAScheduler]
+GO
+
+/****** Object:  Table [dbo].[Role]    Script Date: 2/14/2023 11:23:28 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Role](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](500) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
--- src/Persistence/Scripts/Drop.sql
+++ src/Persistence/Scripts/Drop.sql
@@ -1,9 +1,19 @@
-﻿/***** this query will drop a table (in this instance, 'Users') from the 'JAScheduler' database *****/
+﻿/***** this query will drop a table (in this instance, 'User') from the 'JAScheduler' database *****/
 
 USE [JAScheduler]
 GO
 
-/****** Object:  Table [dbo].[Users]    Script Date: 2/7/2023 9:42:33 AM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
-DROP TABLE [dbo].[Users]
+/****** Object:  Table [dbo].[User]    Script Date: 2/14/2023 11:25:40 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
+DROP TABLE [dbo].[User]
+GO
+
+/***** this query will drop a table (in this instance, 'Role') from the 'JAScheduler' database *****/
+
+USE [JAScheduler]
+GO
+
+/****** Object:  Table [dbo].[Role]    Script Date: 2/14/2023 11:26:22 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Role]') AND type in (N'U'))
+DROP TABLE [dbo].[Role]
 GO
