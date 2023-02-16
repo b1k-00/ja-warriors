8b6ae4adeb45f4e89061e1ce401ef78d0370719f
Brennen Collins <brennen.collins@ruralsourcing.com>
2023-02-16 13:15:36 -0600

    Updating interfaces

---
+++ src/Application/Interfaces/IUserApp.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces;
+public interface IUserApp
+{
+    bool Validate(string username, string password);
+
+    bool CreateUser(User user);
+
+    bool DeleteUser(string username);
+    List<User> GetUsers();
+
+}
