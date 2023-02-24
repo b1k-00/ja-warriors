71dd188218fbdd1abfe190b7a7e381a1c44a8ece
Libby Blair <libby.blair@ruralsourcing.com>
2023-02-24 12:05:59 -0600

    Created MeetingsController, IMeetingApp, MeetingApp, Meeting, MeetingRepository, and altered Program.cs and JuniorAssociateDbContext to include dependency injection for the meetings.

---
+++ src/API/Controllers/MeetingsController.cs
@@ -0,0 +1,42 @@
+﻿using Application;
+using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class MeetingsController : BaseApiController
+{
+    IMeetingApp _meetingApp = null;
+
+    public MeetingsController(IMeetingApp meetingApp)
+    {
+        _meetingApp = meetingApp;
+    }
+
+    [HttpGet("AllMeetings")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Meeting>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<List<Meeting>> AllMeetings()
+    {
+        return await _meetingApp.GetMeetings();
+    }
+
+    [HttpPost("CreateMeeting")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<bool> CreateMeeting(Meeting meeting)
+    {
+
+        return await _meetingApp.CreateMeeting(meeting);
+    }
+
+    //[HttpPost("DeleteMeeting")]
+    //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    //[ProducesResponseType(StatusCodes.Status404NotFound)]
+    //public async Task<bool> DeleteMeeting(string meetingName)
+    //{
+    //    return await _meetingApp.DeleteMeeting(meetingName);
+
+    //}
+}
