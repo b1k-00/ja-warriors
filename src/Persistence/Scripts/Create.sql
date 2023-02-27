65854cae1e797f52c6ba6450083a10f1fd0ae6ec
Brennen Collins <brennen.collins@ruralsourcing.com>
2023-02-27 09:28:13 -0600

    Added the two table scripts in create.sql file

--- src/Persistence/Scripts/Create.sql
+++ src/Persistence/Scripts/Create.sql
@@ -53,3 +53,40 @@ GO
 ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([RoleId])
 REFERENCES [dbo].[Roles] ([Id])
 GO
+
+CREATE TABLE [dbo].[Meetings](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](500) NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+ CONSTRAINT [PK_Meetings] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[UserMeetings](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NULL,
+	[MeetingId] [int] NULL,
+ CONSTRAINT [PK_UserMeetings] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[UserMeetings]  WITH CHECK ADD  CONSTRAINT [FK_UserMeetings_Meetings] FOREIGN KEY([MeetingId])
+REFERENCES [dbo].[Meetings] ([Id])
+GO
+
+ALTER TABLE [dbo].[UserMeetings] CHECK CONSTRAINT [FK_UserMeetings_Meetings]
+GO
+
+ALTER TABLE [dbo].[UserMeetings]  WITH CHECK ADD  CONSTRAINT [FK_UserMeetings_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[UserMeetings] CHECK CONSTRAINT [FK_UserMeetings_Users]
+GO
