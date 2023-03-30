468d8ee99e582e7035c3a2e75abe27fd8237cb9f
Brennen Collins <brennen.collins@teamsparq.com>
2023-03-30 14:08:27 -0500

    added DTO classes & new method for table dashboard

---
+++ src/Domain/DTO/AvailabilitySummaryDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO;
+public class AvailabilitySummaryDTO
+{
+    public int DayOfWeek { get; set; }
+
+    public List<AvailabilitySummaryValueDTO> UserCounts { get; set; }
+
+    public AvailabilitySummaryDTO()
+    {
+        UserCounts = new List<AvailabilitySummaryValueDTO>();
+    }
+}
